3c8ff1fb3fe70f4279f80153bb95a1adcce46e31
Matthew Longshore <matthewlongshore12@gmail.com>
2023-04-11 09:41:59 -0400

    models

--- server/models.py
+++ server/models.py
@@ -5,3 +5,34 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
+
+class Apartment( db.Model, SerializerMixin ):
+    __tablename__ = 'apartments'
+
+    id = db.Column( db.Integer, primary_key=True )
+    number = db.Column( db.Integer )
+
+    leases = db.relationship( 'Lease', backref='Apartment' )
+
+
+
+class Tenant( db.Model, SerializerMixin ):
+    __tablename__ = 'tenants'
+
+    id = db.Column( db.Integer, primary_key=True )
+    name = db.Column( db.String, nullable=False )
+    age = db.Column( db.Integer )
+
+    leases = db.relationship( 'Lease', backref='Tenant' )
+
+
+
+class Lease( db.Model, SerializerMixin ):
+    __tablename__ = 'leases'
+
+    id = db.Column( db.Integer, primary_key=True )
+    rent = db.Column( db.Integer )
+
+    apartment_id = db.Column( db.Integer, db.ForeignKey( 'apartments.id' ) )
+    tenant_id = db.Column( db.Integer, db.ForeignKey( 'tenants.id' ) )
+
